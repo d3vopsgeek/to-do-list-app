070100559d74151318fde79163beea8ebb0fc6cc
d3vopsgeek <41163727+d3vopsgeek@users.noreply.github.com>
2020-04-02 12:34:58 -0600

    Adding update tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
@@ -9,6 +9,20 @@ def index(request):
 
 	form = TaskForm()
 
+	if request.method == 'POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/')
+
+
 	context = {'tasks': tasks, 'form': form}
 	return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+	task = Task.objects.get(id=pk)
+
+	form = TaskForm(instance=task)
+	context = {'form':form}
+	return render(request, 'tasks/update_task.html', context)
 
