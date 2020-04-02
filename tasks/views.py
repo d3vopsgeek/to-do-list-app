bd3fa09fb0dd9b4db881c215f0df8e2768105289
d3vopsgeek <41163727+d3vopsgeek@users.noreply.github.com>
2020-04-02 14:21:10 -0600

    finish functionality before bootstrap

--- tasks/views.py
+++ tasks/views.py
@@ -35,8 +35,13 @@ def updateTask(request, pk):
 
 def deleteTask(request, pk):
 	item = Task.objects.get(id=pk)
+
+	if request.method == 'POST':
+		item.delete()
+		return redirect('/')
+		
 	context = {'item':item}
-	return render(request, 'tasks/delete.html')
+	return render(request, 'tasks/delete.html', context)
 
 
 
